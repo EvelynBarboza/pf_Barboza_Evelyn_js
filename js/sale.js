79fe40270f1d44b2f09aeb515f73fb7e255991ac
EvelynBarboza <natyevy12@gmail.com>
2023-09-02 15:29:01 -0300

    ultimo

--- js/sale.js
+++ js/sale.js
@@ -1,5 +1,5 @@
 
-let url;
+/*let url;
 const link = document.querySelector('.sale');
 //console.log(link)
 
@@ -8,61 +8,28 @@ link.addEventListener('click', setURL);
 function setURL(evt){
     evt.preventDefault();
     url = evt.target.dataset?.pagina
-    if(url != undefined){
-        pedirPagina(url + ".html");
+        if(url != undefined){
+            pedirPagina(url + ".html");
+        }else {
+            pedirPagina('productos.html')
+        }
+    
+    }
+        //pedirPagina(url + ".html");
         //pedirPagina('productos.html');
     /*}else {
-        pedirPagina('productos.html')*/
-    }
-}
+        pedirPagina('productos.html')
 
 function pedirPagina(url){
-    fetch(url)
+    fetch('pages/productos.html')
         .then((res)=>{
             return res.text();
         })
         .then((pagina)=>{
-           document.querySelector('#vacio').innerHTML = pagina;
-        })
-        .catch((err)=>{
-        })
-}
-
-document.addEventListener('DOMContentLoaded', setURL)
-
-//console.log(links)
-
-/*for(let link of links){
-    link.addEventListener('click', setURL)
-}
-
-function setURL(evt){
-    evt.preventDefault();
-    //console.log("click");
-    //console.log(evt.target.dataset.pagina);
-    url = evt.target.dataset?.pagina
-    if(url != undefined){
-        pedirPagina(url + ".html");
-    }else {
-        pedirPagina('inicio.html')
-    }
-
-}
-
-function pedirPagina(url){
-    //console.log(url);
-    fetch(url)
-        .then((res)=>{
-            return res.text();
-        })
-        .then((pagina)=>{
-            //console.log(pagina)
-            document.querySelector('main').innerHTML = pagina;
+          document.querySelector('#vacio').innerHTML = pagina;
         })
         .catch((err)=>{
             alert(err)
         })
 }
-
-
 document.addEventListener('DOMContentLoaded', setURL)*/
